53e14b9b523f26216d01dd40184871ddb6759abb
Johannes Orssich <johannesorssich@Johanness-MacBook-Pro-2.local>
2022-01-24 12:59:54 +0100

    modificacion practica1

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,4 +10,10 @@ public class App
     {
         System.out.println( "Hello World!" );
     }
+
+    public static void main( String[] args )
+    {
+        System.out.println( "Hacemos un cambio" );
+    }
 }
+
